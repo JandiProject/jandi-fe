b8a70cee772f82d71c8f2c25031540afe92bb51d
jungsun <dlwjdtjs0111@gmail.com>
2026-03-23 19:03:02 +0900

    fix: emotion.d.ts import를 triple-slash reference로 변경 후 eslint 규칙 비활성화

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,6 @@
-import "./types/emotion.d.ts";
+/* eslint-disable-next-line @typescript-eslint/triple-slash-reference */
+/// <reference path="./types/emotion.d.ts" />
+
 export * from "./components";
 export { GlobalStyles } from "./GlobalStyles";
 export {
